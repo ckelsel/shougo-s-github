bc6fd7b0dbc02e614063da1155c9cb30f606a149
Your Name <you@example.com>
2018-09-15 22:05:49 +0800

    update script

--- build-vim.sh
+++ build-vim.sh
@@ -2,7 +2,7 @@
 
 #ubuntu 14.04
 sudo apt-get install lua5.1  liblua5.1-0-dev luajit libluajit-5.1
-sudo apt-get install python3.4-dev
+sudo apt-get install python3.4-dev zsh
 
 sudo apt-get install python3-pip
 sudo pip3 install typing
