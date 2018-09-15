a5bf14591299a6f238faafcc3a1c84846f2116eb
Your Name <you@example.com>
2018-09-15 14:41:14 +0800

    missing depends

--- build-vim.sh
+++ build-vim.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+#ubuntu 14.04
 sudo apt-get install lua5.1  liblua5.1-0-dev luajit libluajit-5.1
 sudo apt-get install python3.4-dev
+
+sudo apt-get install python3-pip
+sudo pip3 install typing
 
 ./configure \
 --with-features=huge \
