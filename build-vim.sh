f8267fb09069ba66bf9d06ec1827b1bdc78d6cd3
Your Name <you@example.com>
2018-09-15 13:58:24 +0800

    update build vim script

--- build-vim.sh
+++ build-vim.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+sudo apt-get install lua5.1  liblua5.1-0-dev luajit libluajit-5.1
+sudo apt-get install python3.4-dev
+
 ./configure \
 --with-features=huge \
---with-compiledby="Shougo" \
+--with-compiledby="ckelsel" \
 --enable-multibyte \
 --enable-gui=gtk3 \
---enable-rubyinterp \
 --enable-python3interp \
 --enable-luainterp \
 --with-lua-prefix=/usr --with-luajit \
